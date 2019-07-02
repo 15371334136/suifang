1f6307edb4da60e5d8f573ed8236f10dde67697d
15371334136 <1159512357@qq.com>
2019-07-02 10:37:38 +0800

    suifang
    suifangsuifang

---
+++ files/讨论问题/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[_(W,X,Y,j,Z,ba,q,bb,bc,bb,bd,be,v,_(bf,_(bg,bh,bi,bj),w,bk,bl,_(bm,bn,bo,bp)),S,_(),bq,_(),br,g)])),bs,_(),bt,_(bu,_(bv,bw)));}; 
+var b="url",c="讨论问题.html",d="generationDate",e=new Date(1562035016200.63),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="bc729a57590f48a09aea9e6bb74fd72f",q="type",r="Axure:Page",s="name",t="讨论问题",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="id",X="8e77a0f31a3e49a190aeb4a7d55a9376",Y="label",Z="friendlyType",ba="矩形",bb="vectorShape",bc="styleType",bd="visible",be=true,bf="size",bg="width",bh=792,bi="height",bj=281,bk="2285372321d148ec80932747449c36c9",bl="location",bm="x",bn=12,bo="y",bp=8,bq="imageOverrides",br="generateCompound",bs="masters",bt="objectPaths",bu="8e77a0f31a3e49a190aeb4a7d55a9376",bv="scriptId",bw="u83";
+return _creator();
+})());
